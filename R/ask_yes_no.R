6ee6f87f4aa9985376916da3f7fc73427f22d017
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-03-03 16:53:53 +0100

    ✨ Add ask_yes_no()

---
+++ R/ask_yes_no.R
@@ -0,0 +1,28 @@
+#' Function to ask a simple yes no question
+#' Provides a simple wrapper around `utils::askYesNo()`.
+#' This function is used to ask questions in an interactive way.
+#' It repeats the question until a valid answer is given.
+#' @inheritParams utils::askYesNo
+#' @importFrom utils askYesNo
+#' @export
+#' @family questions
+ask_yes_no <- function(
+  msg,
+  default = TRUE,
+  prompts = c("Yes", "No", "Cancel"),
+  ...
+) {
+  if (!interactive()) {
+    return(default)
+  }
+  assert_that(is.string(msg), noNA(msg))
+  answer <- try(askYesNo(msg = msg, default = default, prompts = prompts))
+  while (inherits(answer, "try-error") || is.null(answer)) {
+    sprintf("`%s`", prompts) |>
+      paste(collapse = ", ") |>
+      sprintf(fmt = "Please answer with %s.") |>
+      warning(immediate. = TRUE, call. = FALSE)
+    answer <- try(askYesNo(msg = msg, default = default, prompts = prompts))
+  }
+  return(answer)
+}

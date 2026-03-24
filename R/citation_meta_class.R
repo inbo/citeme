fd597a1232d830d2df8ebd84e41843b1d249e7b1
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-03-24 10:28:27 +0100

    ✨ Export validate_citation()

--- R/citation_meta_class.R
+++ R/citation_meta_class.R
@@ -181,45 +181,6 @@ citation_print <- function(errors, meta, notes, path, person, warnings) {
   }
 }
 
-#' @importFrom assertthat assert_that
-validate_citation <- function(meta) {
-  assert_that(inherits(meta, "citation_meta"))
-  org <- org_list$new()$read(meta$get_path)
-  persons <- meta$get_person
-  rightsholder <- persons[vapply(
-    persons$role,
-    FUN = function(x) {
-      "cph" %in% x
-    },
-    FUN.VALUE = logical(1)
-  )]
-  funder <- persons[vapply(
-    persons$role,
-    FUN = function(x) {
-      "fnd" %in% x
-    },
-    FUN.VALUE = logical(1)
-  )]
-  contact <- any("cre" %in% unlist(persons$role))
-  c(rightsholder$email, funder$email) |>
-    unlist() |>
-    unique() |>
-    org$get_zenodo_by_email() -> required_communities
-  org$validate_person(persons, lang = meta$get_meta$language) |>
-    attr("errors") |>
-    c(
-      org$validate_rules(rightsholder = rightsholder, funder = funder),
-      sprintf(
-        "missing required Zenodo community `%s`",
-        paste(required_communities, collapse = ", ")
-      )[
-        length(required_communities) > 0 &&
-          !all(required_communities %in% meta$get_meta$community)
-      ],
-      "no individual with `corresponding: true` or role `cre`"[!contact]
-    )
-}
-
 #' @importFrom assertthat assert_that has_name
 #' @importFrom fs path path_rel
 #' @importFrom jsonlite toJSON

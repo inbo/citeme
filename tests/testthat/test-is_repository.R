7e5fe8f342599a6ada8156a86b0279503be23219
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-03-24 09:54:01 +0100

    ✅ Fix unit test

--- tests/testthat/test-is_repository.R
+++ tests/testthat/test-is_repository.R
@@ -1,8 +1,3 @@
-test_that("is_repository returns TRUE for git repository", {
-  # The current working directory should be a git repository
-  expect_true(is_repository("."))
-})
-
 test_that("is_repository returns FALSE for non-git directory", {
   temp_dir <- tempdir()
   non_git_dir <- file.path(temp_dir, "test_non_git")
@@ -10,4 +5,7 @@ test_that("is_repository returns FALSE for non-git directory", {
   on.exit(unlink(non_git_dir, recursive = TRUE))
 
   expect_false(is_repository(non_git_dir))
+
+  gert::git_init(non_git_dir)
+  expect_true(is_repository(non_git_dir))
 })

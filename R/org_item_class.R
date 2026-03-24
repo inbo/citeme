98aba2e12ecbefc98f6764be851f8b4edd56e288
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-03-24 10:25:51 +0100

    ♻️ Remove duplicate function

--- R/org_item_class.R
+++ R/org_item_class.R
@@ -365,45 +365,6 @@ org_item <- R6Class(
   )
 )
 
-validate_license <- function(license) {
-  stopifnot(
-    "`license` must be a list" = inherits(license, "list"),
-    # fmt:skip
-    "`license` must contain `package`, `project`, and `data`" =
-      all(c("package", "project", "data") %in% names(license)),
-    "`license` must contain character vectors" = all(
-      vapply(license, is.character, FUN.VALUE = logical(1))
-    ),
-    "`license` must contain named vectors" = all(
-      vapply(
-        license,
-        function(x) {
-          length(x) == 0 || (!is.null(names(x)) && all(names(x) != ""))
-        },
-        FUN.VALUE = logical(1)
-      )
-    ),
-    "`license` must contain uniquely named vectors" = all(
-      vapply(
-        license,
-        function(x) {
-          length(x) == 0 || anyDuplicated(names(x)) == 0
-        },
-        FUN.VALUE = logical(1)
-      )
-    ),
-    "`license` must contain vectors with unique licenses" = all(
-      vapply(
-        license,
-        function(x) {
-          length(x) == 0 || anyDuplicated(x) == 0
-        },
-        FUN.VALUE = logical(1)
-      )
-    )
-  )
-}
-
 set_non_empty <- function(x, fun, prompt) {
   if (x == "") {
     return(x)

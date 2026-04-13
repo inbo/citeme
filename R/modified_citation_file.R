440f653db756f026a56b01dc3550c1a0d16f233b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-04-13 10:00:51 +0200

    🐛 Fix modified_citation_file()

--- R/modified_citation_file.R
+++ R/modified_citation_file.R
@@ -1,5 +1,6 @@
 modified_citation_file <- function(citation_file, base_path) {
-  paste0("/", base_path) |>
+  normalizePath(base_path, mustWork = TRUE, winslash = "/") |>
+    paste0("/") |>
     gsub("", citation_file) |>
     is_tracked_not_modified(base_path) -> unchanged
   paste(
